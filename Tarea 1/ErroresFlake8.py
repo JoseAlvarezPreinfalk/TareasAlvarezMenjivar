41ec80cdcee96fc8af6a923bad05efd2474512d4
MMenjivar21 <114839475+MMenjivar21@users.noreply.github.com>
2025-08-14 19:53:54 -0600

    Corrección de errores Flake8

--- Tarea 1/ErroresFlake8.py
+++ Tarea 1/ErroresFlake8.py
@@ -2,10 +2,10 @@
 
 # 1) Indentación no es un múltiplo de 4 (cantidad de espacios)
 if 1 < 2:
-  print("Error E111 en Flake8")
+    print("Se corrige el error E111 en Flake8")
 
 # 2) Línea de código excede los 79 caracteres
-print("La línea de código contiene más de 79 caracteres, causando un error E501")
+print("La línea de código ya no excede 79 caracteres, evitando un error E501")
 
 # 3) Espacios en blanco luego del último caracter
-print("Los espacio luego del print causan un error W291")   
+print("Ya no hay espacios luego del print, no causan un error W291")
